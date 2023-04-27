84a9958e7dc95ad1c59c595382bd9a71beeb6e2c
Abhishek Kumar Singh <abhihsekse@outlook.com>
2023-04-27 18:46:48 +0530

    added books data

---
+++ src/books.js
@@ -0,0 +1,109 @@
+//array containing books data
+import img1 from "./images/book-1.jpg";
+import img2 from "./images/book-2.jpg";
+import img3 from "./images/book-3.jpg";
+import img4 from "./images/book-4.jpg";
+import img5 from "./images/book-5.jpg";
+import img6 from "./images/book-6.jpg";
+import img7 from "./images/book-7.jpg";
+import img8 from "./images/book-8.jpg";
+import img9 from "./images/book-9.jpg";
+import img10 from "./images/book-10.jpg";
+import img11 from "./images/book-11.jpg";
+import img12 from "./images/book-12.jpg";
+import img13 from "./images/book-13.jpg";
+import img14 from "./images/book-14.jpg";
+import img15 from "./images/book-15.jpg";
+
+export const books = [
+  {
+    id: 1,
+    author: "Morgan Housel",
+    title: "The Psychology of Money",
+    img: img1,
+  },
+  {
+    id: 2,
+    author: "Akshat Gupta",
+    title: "The Hidden Hindu",
+    img: img2,
+  },
+  {
+    id: 3,
+    author: "James Clear",
+    title: "Atomic Habits",
+    img: img3,
+  },
+  {
+    id: 4,
+    author: "Francesc Miralles",
+    title: "Ikigai",
+    img: img4,
+  },
+  {
+    id: 5,
+    author: "Sudha Murty",
+    title: "Grandma's Bag of Stories",
+    img: img5,
+  },
+  {
+    id: 6,
+    author: "Wonder House Books",
+    title: "My First Library",
+    img: img6,
+  },
+  {
+    id: 7,
+    author: "The Hidden Hindu, Hindi",
+    title: "Akshat Gupta",
+    img: img7,
+  },
+  {
+    id: 8,
+    author: "Maple Press",
+    title: "Brain Activity Book for Kids",
+    img: img8,
+  },
+  {
+    id: 9,
+    author: "Nikhil Kumar Gupta",
+    title: "BlackBook of English Vocabulary",
+    img: img9,
+  },
+  {
+    id: 10,
+    author: "Dr. Sakshi Arora Hans",
+    title: "ONE Touch Obstetrics & Gynecology",
+    img: img10,
+  },
+  {
+    id: 11,
+    author: "Gaur Gopal Das",
+    title: "Energize Your Mind",
+    img: img11,
+  },
+  {
+    id: 12,
+    author: "Robert T. Kiyosaki",
+    title: "Rich Dad Poor Dad",
+    img: img12,
+  },
+  {
+    id: 13,
+    author: "Wonder House Books",
+    title: "My First Book of Pencil Control",
+    img: img13,
+  },
+  {
+    id: 14,
+    author: "His Divine Grace A. C. Bhaktivedanta Swami",
+    title: "Basics of Bhagavad Gita",
+    img: img14,
+  },
+  {
+    id: 15,
+    author: "Maple Press",
+    title: "My First Mythology Tale",
+    img: img15,
+  },
+];
